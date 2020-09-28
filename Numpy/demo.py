e3487bc495b38474e364b1a1feb1a6ee03b70401
Payne <wuzhipeng1289690157@gmail.com>
2020-09-29 02:31:53 +0800

    CoderWork

---
+++ Numpy/demo.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+L = list(i for i in range(10))
+
+# t1 = np.array(L)
+t1 = np.array(range(16)).reshape((2,2,4))
+print(t1)
+t2 = np.arange(10 ** 2, dtype=int)
+print(t2)
---
+++ README.md
@@ -0,0 +1,1 @@
+# PayneAnalysisData Code
